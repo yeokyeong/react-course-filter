b939fb081f642b87ec8f055e8e7fabd1b67f636e
= <yeokyeong1004@gmail.com>
2022-08-11 17:00:19 +0900

    [kate] add mobile design

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./navBar.scss";
 import logoImg from "../assets/logo.svg";
+
 export const NavBar: React.FC = () => {
   return (
-    <div className="nav-bar">
-      <div className="nav-bar--wrapper">
+    <div className="nav-bar--wrapper">
+      <div className="nav-bar">
         <img src={logoImg} alt="logo" />
       </div>
     </div>
