28584179162a26567d2f71828387557e3df6d649
= <yeokyeong1004@gmail.com>
2022-08-11 15:34:37 +0900

    [kate] change file type

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Main from "./pages/main";
+import { NavBar } from "./components/navBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
